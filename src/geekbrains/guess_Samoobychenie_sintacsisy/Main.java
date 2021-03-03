a2efbc83009c4716569287fbf7e0ac1ac3327fe4
IgorK55 <egor2008@mail.ru>
2021-03-03 16:02:52 +0200

    Послано в GitHub.

---
+++ src/geekbrains/guess_Samoobychenie_sintacsisy/Main.java
@@ -0,0 +1,16 @@
+package geekbrains.guess_Samoobychenie_sintacsisy;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	String q = ("Как тебя зовут?");
+	     int a = 12;
+	     String name = "Nick";
+	     String x = "сколько лет?";
+	 System.out.println(q);
+		System.out.println(name);
+		System.out.println(x);
+		System.out.println(a);
+		System.out.println("Mолодец,хороший мальчик!");
+    }
+}
